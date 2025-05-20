08e629adf0210aadb22da5dc5fcc1310579cdc5f
IvIdz03 <ividzano@vsite.hr>
2025-05-20 19:02:06 +0200

    prepravljeno

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,47 @@
 
 namespace vsite::oop::v4
 {
+	unsigned int insect::legs() const {
+		return 6;
+	}
+	unsigned int bird::legs() const {
+		return 2;
+	}
+	unsigned int spider::legs() const {
+		return 8;
+	}
+	std::string cockroach::species() const {
+		return "cockroach";
+	}
+	std::string tarantula::species() const {
+		return "tarantula";
+	}
+	std::string sparrow::species() const {
+		return "sparrow";
+	}
 
+	std::string leg_counter::add_animal(animal* an)
+	{
+		l += an->legs();
+		return an->species();
+	}
+
+	unsigned int leg_counter::legs() {
+		return l;
+	}
+
+	std::unique_ptr<animal> animal_factory(const int animal_number)
+	{
+		switch (animal_number)
+		{
+		case 1:
+			return std::make_unique<cockroach>();
+		case 2:
+			return std::make_unique<sparrow>();
+		case 3:
+			return std::make_unique<tarantula>();
+		default:
+			return nullptr;
+		}
+	}
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,51 @@
 #include <string>
 #include <memory>
 
-namespace vsite::oop::v4 
+namespace vsite::oop::v4
 {
+	class animal {
+	public:
+		virtual std::string species() const = 0;
+		virtual unsigned int legs() const = 0;
+	};
 
+	class insect : public animal {
+	public:
+		unsigned int legs() const override;
+	};
+
+	class bird : public animal {
+	public:
+		unsigned int legs() const override;
+	};
+
+	class spider : public animal {
+	public:
+		unsigned int legs() const override;
+	};
+
+	class tarantula : public spider {
+	public:
+		std::string species() const override;
+	};
+
+	class cockroach : public insect {
+	public:
+		std::string species() const override;
+	};
+
+	class sparrow : public bird {
+	public:
+		std::string species() const override;
+	};
+
+	class leg_counter {
+	public:
+		std::string add_animal(animal* an);
+		unsigned int legs();
+	private:
+		unsigned int l = 0;
+	};
+
+	std::unique_ptr<animal> animal_factory(const int animal_number);
 }
